ead32831e7a888b609e26a48e97c74a290de4325
billyhkim <billson.kim@gmail.com>
2019-06-26 23:37:59 +0000

    added styling to product grid for responsiveness

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardImg, CardBody, CardSubtitle, Button, Col } from 'reactstrap';
 
 export default class ProductListItem extends React.Component {
   constructor(props) {
@@ -12,25 +12,17 @@ export default class ProductListItem extends React.Component {
   }
   render() {
     return (
-      // <div className="col-sm-4 card-deck">
-      //   <div className="card">
-      //     <img src={this.props.item.image} alt={this.props.item.name} className="card-img-top item-image"/>
-      //     <div className="card-body">
-      //       <h4>{this.props.item.name}</h4>
-      //       <h5 className="text-muted">${(this.props.item.price / 100).toFixed(2)}</h5>
-      //       <div className="description">{this.props.item.price}</div>
-      //     </div>
-      //   </div>
-      // </div>
-      <Card>
-        <CardImg top width="100%" src={this.props.item.image} alt={this.props.item.name} />
-        <CardBody>
-          <CardTitle>{this.props.items.brand} {this.props.items.name}</CardTitle>
-          <CardSubtitle>{this.props.items.colorway}</CardSubtitle>
-          <CardSubtitle>{(this.props.items.price / 100).toFixed(2)}</CardSubtitle>
-          <Button>MORE INFO</Button>
-        </CardBody>
-      </Card>
+      <Col sm="4" className="mt-2 mb-2 d-flex align-items-stretch">
+        <Card className="card-font text-center">
+          <CardImg top width="100%" src={this.props.item.image} alt={this.props.item.name} />
+          <CardBody>
+            <CardSubtitle className="h6 text-muted mb-1">{this.props.item.brand} {this.props.item.name}</CardSubtitle>
+            <CardSubtitle className="h2 mb-1">{(this.props.item.colorway).toUpperCase()}</CardSubtitle>
+            <CardSubtitle className="h4 mb-1">{'$' + (this.props.item.price / 100).toFixed(2)}</CardSubtitle>
+            <Button>MORE INFO</Button>
+          </CardBody>
+        </Card>
+      </Col>
     );
   }
 }
--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Row } from 'reactstrap';
 import ProductListItem from './product-list-item';
 
 export default class ProductList extends React.Component {
   render() {
     const items = this.props.products.map(item => <ProductListItem key={item.id} item={item} setView={this.props.setView}/>);
     return (
-      <Container>
-        <Row>
-          {items}
-        </Row>
-      </Container>
+      <Row className="justify-content-md-center mr-1 ml-1">
+        {items}
+      </Row>
     );
   }
 }
