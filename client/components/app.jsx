5ee029ec95aa6172b82c3ec469fc106aae3df351
billyhkim <billson.kim@gmail.com>
2019-06-26 06:06:09 +0000

    updated app.jsx to return null after reactstrap testing

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Button color="danger">Danger!</Button>
+      null
     );
   }
 }
