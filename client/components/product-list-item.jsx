bac583483ca9c3d6f3e7c131a4727d9ce8a1815b
billyhkim <billson.kim@gmail.com>
2019-07-17 03:06:26 +0000

    added fallback image for catalog items and logic

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,17 +4,30 @@ import { Button, Card, CardBody, CardImg, CardSubtitle, Col } from 'reactstrap';
 export default class ProductListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fallbackImageError: true
+    };
     this.handleSetView = this.handleSetView.bind(this);
+    this.addFallbackSrc = this.addFallbackSrc.bind(this);
   }
   handleSetView(e) {
     e.preventDefault();
     this.props.setView('details', { id: this.props.item.id });
   }
+  addFallbackSrc(e) {
+    // prevents infinite callback loop if fallback also fails
+    if (this.state.fallbackImageError) {
+      this.setState({
+        fallbackImageError: false
+      });
+      e.target.src = './media/fallback.png';
+    }
+  }
   render() {
     return (
       <Col xs="12" sm="6" md="4" lg="3" className="mt-2 mb-2 d-flex align-items-stretch">
         <Card className="card-font text-center shadow-sm">
-          <CardImg top width="100%" src={this.props.item.image} alt={this.props.item.name} />
+          <CardImg top width="100%" src={this.props.item.image} alt={this.props.item.name} onError={this.addFallbackSrc} />
           <CardBody>
             <CardSubtitle className="h6 text-muted mb-1">{this.props.item.name}</CardSubtitle>
             <CardSubtitle className="h2 mb-1">{(this.props.item.colorway).toUpperCase()}</CardSubtitle>
